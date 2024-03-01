6e24417f27393611dccc074a6563c258feb6d475
Yakup Zengin <clafedersin@gmail.com>
2024-03-01 16:01:16 +0300

    created jpa-repository files

---
+++ src/main/java/com/project/questapp/repos/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.questapp.repos;
+
+import com.project.questapp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
