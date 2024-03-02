ad57913f3314a1941e1b76b939d351bbc6ca6c18
Yakup Zengin <clafedersin@gmail.com>
2024-03-02 22:21:13 +0300

    created PostService

---
+++ src/main/java/com/project/questapp/services/PostService.java
@@ -0,0 +1,62 @@
+package com.project.questapp.services;
+
+import com.project.questapp.entities.Post;
+import com.project.questapp.entities.User;
+import com.project.questapp.repos.PostRepository;
+import com.project.questapp.requests.PostCreateRequest;
+import com.project.questapp.requests.PostUpdateRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    private PostRepository postRepository;
+    private UserService userService;
+
+    public PostService(PostRepository postRepository,UserService userService) {
+        this.postRepository = postRepository;
+        this.userService = userService;
+    }
+
+    public List<Post> getAllPosts(Optional<Long> userId) {
+        if (userId.isPresent())
+            return postRepository.findByUserId(userId.get());
+        return postRepository.findAll();
+    }
+
+    public Post getOnePostById(Long postId) {
+        return postRepository.findById(postId).orElse(null);
+    }
+
+    public Post createOnePost(PostCreateRequest newPostRequest) {
+        User user = userService.getOneUser(newPostRequest.getUserId());
+        if (user == null) {
+            return null;
+        }
+        Post toSavePost = new Post();
+        toSavePost.setId(newPostRequest.getId());
+        toSavePost.setTitle(newPostRequest.getTitle());
+        toSavePost.setText(newPostRequest.getText());
+        toSavePost.setUser(user);
+
+        return postRepository.save(toSavePost);
+    }
+
+    public Post updateOnePostById(Long postId, PostUpdateRequest updatePost) {
+        Optional<Post> post = postRepository.findById(postId);
+        if (post.isPresent()){
+            Post toUpdatePost = post.get();
+            toUpdatePost.setText(updatePost.getText());
+            toUpdatePost.setTitle(updatePost.getTitle());
+            postRepository.save(toUpdatePost);
+            return toUpdatePost;
+        }
+        return null;
+    }
+
+    public void deleteOnePostById(Long postId) {
+        postRepository.deleteById(postId);
+    }
+}
