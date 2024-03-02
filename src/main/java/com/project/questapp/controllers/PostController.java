01852fe052fd7661501e5b6d31a22ed40d567809
Yakup Zengin <clafedersin@gmail.com>
2024-03-02 22:19:48 +0300

    created PostController

---
+++ src/main/java/com/project/questapp/controllers/PostController.java
@@ -0,0 +1,43 @@
+package com.project.questapp.controllers;
+
+import com.project.questapp.entities.Post;
+import com.project.questapp.requests.PostCreateRequest;
+import com.project.questapp.requests.PostUpdateRequest;
+import com.project.questapp.services.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts(@RequestParam Optional<Long> userId){
+        return postService.getAllPosts(userId);
+    }
+    @PostMapping
+    public Post createOnePost(@RequestBody PostCreateRequest newPostRequest){
+        return postService.createOnePost(newPostRequest);
+    }
+
+    @GetMapping("/{postId}")
+    public Post getOnePost(@PathVariable Long postId) {
+        return postService.getOnePostById(postId);
+    }
+
+    @PutMapping("/{postId}")
+    public Post updateOnePost(@PathVariable Long postId,@RequestBody PostUpdateRequest updatePost){
+        return postService.updateOnePostById(postId, updatePost);
+    }
+    @DeleteMapping("/{postId}")
+    public void deleteOnePost(@PathVariable Long postId) {
+         postService.deleteOnePostById(postId);
+    }
+}
