5ec1b09a3376ce6d3d1b6eadfdd281e3a1f29c5d
Yakup Zengin <clafedersin@gmail.com>
2024-03-03 18:01:46 +0300

    Added LikeRepository interface for managing Like entities using JpaRepository.

--- src/main/java/com/project/questapp/repos/LikeRepository.java
+++ src/main/java/com/project/questapp/repos/LikeRepository.java
@@ -1,7 +1,16 @@
 package com.project.questapp.repos;
 
+import com.project.questapp.entities.Comment;
 import com.project.questapp.entities.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    List<Like> findByUserIdAndPostId(Optional<Long> userId, Optional<Long> postId);
+
+    List<Like> findByUserId(Long userId);
+
+    List<Like> findByPostId(Long postId);
 }
