7281d84582f9c924aed978f57fafa0b0377d6eb9
Yakup Zengin <clafedersin@gmail.com>
2024-03-07 00:25:26 +0300

    Update PostService to include LikeService for managing likes

--- src/main/java/com/project/questapp/services/PostService.java
+++ src/main/java/com/project/questapp/services/PostService.java
@@ -1,11 +1,14 @@
 package com.project.questapp.services;
 
+import com.project.questapp.entities.Like;
 import com.project.questapp.entities.Post;
 import com.project.questapp.entities.User;
 import com.project.questapp.repos.PostRepository;
 import com.project.questapp.requests.PostCreateRequest;
 import com.project.questapp.requests.PostUpdateRequest;
+import com.project.questapp.responses.LikeResponse;
 import com.project.questapp.responses.PostResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,18 +19,26 @@ import java.util.stream.Collectors;
 public class PostService {
     private PostRepository postRepository;
     private UserService userService;
+    private LikeService likeService;
 
     public PostService(PostRepository postRepository,UserService userService) {
         this.postRepository = postRepository;
         this.userService = userService;
     }
-
+    @Autowired
+    public void setLikeService(LikeService likeService) {
+        this.likeService = likeService;
+    }
     public List<PostResponse> getAllPosts(Optional<Long> userId) {
         List<Post> list;
-        if (userId.isPresent())
-            list =  postRepository.findByUserId(userId.get());
-        list =  postRepository.findAll();
-        return list.stream().map(p -> new PostResponse(p)).collect(Collectors.toList());
+        if (userId.isPresent()) {
+            list = postRepository.findByUserId(userId.get());
+         } else {
+            list =  postRepository.findAll();
+        }
+        return list.stream().map(p -> {
+            List<LikeResponse> likes = likeService.getAllLikesWithParam(Optional.ofNullable(null), Optional.of(p.getId()));
+            return new PostResponse(p, likes);}).collect(Collectors.toList());
     }
 
     public Post getOnePostById(Long postId) {
