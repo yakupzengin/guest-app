52c72d959aa352b462d671bb27cff22e12e210d7
Yakup Zengin <clafedersin@gmail.com>
2024-03-07 00:25:46 +0300

    Update PostResponse class to include like responses

--- src/main/java/com/project/questapp/responses/PostResponse.java
+++ src/main/java/com/project/questapp/responses/PostResponse.java
@@ -1,7 +1,10 @@
 package com.project.questapp.responses;
 
+import com.project.questapp.entities.Like;
 import com.project.questapp.entities.Post;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class PostResponse {
@@ -10,12 +13,14 @@ public class PostResponse {
     String userName;
     String text;
     String title;
+    List<LikeResponse> postLikes;
 
-    public PostResponse(Post entity){
+    public PostResponse(Post entity, List<LikeResponse> likes){
     this.id=entity.getId();
     this.userId=entity.getUser().getId();
     this.userName=entity.getUser().getUserName();
     this.title=entity.getTitle();
     this.text=entity.getText();
+    this.postLikes = likes;
     }
 }
